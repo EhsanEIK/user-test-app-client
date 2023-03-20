1670f6bc89d7158197fd3a29ebab5f87cf89c6eb
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2023-03-20 21:12:24 +0600

    added update page and done with functionality

--- src/components/Users/UpdateUser.js
+++ src/components/Users/UpdateUser.js
@@ -1,31 +1,59 @@
-import React from 'react';
+import { React, useState } from 'react';
+import { toast } from 'react-hot-toast';
 import { useLoaderData } from 'react-router-dom';
 
 const UpdateUser = () => {
     const user = useLoaderData();
+    const [updateUser, setUpdateUser] = useState(user);
+
+    const handleUpdateUser = id => {
+        event.preventDefault();
+
+        fetch(`http://localhost:5000/users/${id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updateUser)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    toast.success('Data update is successfull');
+                }
+            })
+    }
+
+    const handleInputChange = event => {
+        const field = event.target.name;
+        const value = event.target.value;
+        const newUser = { ...updateUser };
+        newUser[field] = value;
+        setUpdateUser(newUser);
+    }
 
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form className="card-body">
+                    <form onSubmit={() => handleUpdateUser(updateUser?._id)} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Name</span>
                             </label>
-                            <input type="text" value={user?.userName} placeholder="name" name='userName' className="input input-bordered" />
+                            <input onChange={handleInputChange} type="text" defaultValue={updateUser?.userName} placeholder="name" name='userName' className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" value={user?.email} placeholder="email" name='email' className="input input-bordered" />
+                            <input onChange={handleInputChange} type="text" defaultValue={updateUser?.email} placeholder="email" name='email' className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Age</span>
                             </label>
-                            <input type="text" value={user?.age} placeholder="age" name='age' className="input input-bordered" />
+                            <input onChange={handleInputChange} type="text" defaultValue={user?.age} placeholder="age" name='age' className="input input-bordered" />
                         </div>
                         <div className="form-control mt-6">
                             <button className="btn btn-primary">Update</button>
